523ed3510a9664976fd018b571d66515f95b55ab
ha2738 <144284706+ha2738@users.noreply.github.com>
2023-09-14 21:00:52 -0400

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -3,6 +3,7 @@ from socket import *
 # In order to terminate the program
 import sys
 
+
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -14,46 +15,55 @@ def webServer(port=13331):
 
     while True:
         # Establish the connection
-        #print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept()  # Accept the incoming connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()
 
         try:
-            # Receive the client's request
-            message = connectionSocket.recv(1024).decode()
-            filename = message.split()[1]
-            # Open the client requested file
-            f = open(filename[1:])
-            # Prepare the HTTP response headers
-            outputdata = f.read()
-            f.close()
-            #"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+            message = connectionSocket.recv(1024).decode()  # Receive the HTTP request from the client
 
-            # Send the headers to the client
-            connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".encode())
+            # Check if the message contains headers
+            if "HTTP/1.1" in message:
+                filename = message.split()[1]
 
-            # Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
+                # Open the client requested file.
+                try:
+                    f = open(filename[1:], 'rb')
+                    file_data = f.read()
+                    f.close()
 
-            connectionSocket.send("\r\n".encode())
-            # Close the connection socket
-            connectionSocket.close()
+                    # Prepare HTTP response headers
+                    response_headers = "HTTP/1.1 200 OK\r\n"
+                    response_headers += "Server: MyWebServer\r\n"
+                    response_headers += "Connection: close\r\n"
+                    response_headers += "Content-Type: text/html; charset=UTF-8\r\n"
+                    response_headers += f"Content-Length: {len(file_data)}\r\n\r\n"
 
-        except FileNotFoundError:
-            # If the file is not found, send a 404 error response
-           # response_header = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
-            #error_message = "<html><body><h1>404 Not Found</h1></body></html>"
-            connectionSocket.send("HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".encode())
-            connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
+                    # Send the headers
+                    connectionSocket.send(response_headers.encode())
 
-            # Close the connection socket
+                    # Send the content of the requested file to the client
+                    connectionSocket.send(file_data)
+
+                except FileNotFoundError:
+                    # Send a 404 Not Found response
+                    not_found_response = "HTTP/1.1 404 Not Found\r\n"
+                    not_found_response += "Server: MyWebServer\r\n"
+                    not_found_response += "Connection: close\r\n\r\n"
+                    connectionSocket.send(not_found_response.encode())
+
+            else:
+                # Send a response with headers to handle invalid requests
+                invalid_response = "HTTP/1.1 400 Bad Request\r\n"
+                invalid_response += "Server: MyWebServer\r\n"
+                invalid_response += "Connection: close\r\n\r\n"
+                connectionSocket.send(invalid_response.encode())
+
+            # Close client socket
             connectionSocket.close()
 
         except Exception as e:
-            # Handle other exceptions
-            print("Error:", str(e))
-    serverSocket.close()
-    sys.exit()
+            print("Error:", e)
+
 
 if __name__ == "__main__":
     webServer(13331)
